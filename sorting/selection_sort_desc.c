98bf491e2e2ed318025bded1daf5c2a30bfe74de
sagarkarra <sagarkarra06@gmail.com>
2026-06-04 14:22:56 +0530

    adding bubble,selection sort codes

---
+++ sorting/selection_sort_desc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void main()
+{
+    int a[10],n,i,j,temp,max_pos;
+    printf("Enter the number of variables to be used:");
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the value of a[%d]:",i);
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        max_pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if (a[j]>a[max_pos])
+            {
+              max_pos=j;
+            }
+        }
+        temp=a[i];
+        a[i]=a[max_pos];
+        a[max_pos]=temp;
+    }
+    
+    printf("Sorted array is:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+}
